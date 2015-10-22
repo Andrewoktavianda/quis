257a4610d99c5eadaf4ea47b621a29595394d6d2
andrewoktavianda <andrevianda6@gmai.com>
2015-10-22 20:30:57 +0700

    andrew

---
+++ src/PERSEGIPANJANGG.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author endy
+ */
+public class PERSEGIPANJANGG {
+    public int persegipanjang(int panjang,int lebar){
+        int hasil = panjang*lebar;
+        return hasil;
+    }
+    
+    
+}
---
+++ src/SEGITIGAA.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author endy
+ */
+public class SEGITIGAA {
+    public double segitiga(double alas,double tinggi){
+        double hasil = alas*tinggi/2;
+        return hasil;
+    }
+    
+}
